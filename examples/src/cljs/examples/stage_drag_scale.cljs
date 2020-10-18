338e3e76a10857e12880ddf10ca05b0b4a2a653e
ertugrulcetin <ertu.ctn@gmail.com>
2020-10-18 23:48:51 +0300

    add: limit-panning

--- examples/src/cljs/examples/stage_drag_scale.cljs
+++ examples/src/cljs/examples/stage_drag_scale.cljs
@@ -4,6 +4,13 @@
             [konva-cljs.components :as c]))
 
 
+(defn- limit-panning [v]
+  (let [limit 1000]
+    (if (> (js/Math.abs v) limit)
+      (if (pos? v) limit (- limit))
+      v)))
+
+
 (defn- wheel [e state]
   (let [stage (.getStage (.-target e))
         e     (.-evt e)]
@@ -20,14 +27,23 @@
             new-scale   (if (<= new-scale 0.15) 0.15 new-scale)]
         (swap! state assoc
                :scale new-scale
-               :x (* (- (- (:x mouse-point)
-                           (/ (.-x pointer) new-scale))) new-scale)
-               :y (* (- (- (:y mouse-point)
-                           (/ (.-y pointer) new-scale))) new-scale)))
+               :x (limit-panning
+                   (* (- (- (:x mouse-point)
+                            (/ (.-x pointer) new-scale))) new-scale))
+               :y (limit-panning
+                   (* (- (- (:y mouse-point)
+                            (/ (.-y pointer) new-scale))) new-scale))))
       (do
-        (swap! state update :x - (.-deltaX e))
-        (swap! state update :y - (.-deltaY e))))
+        (swap! state update :x (fn [x] (limit-panning (- x (.-deltaX e)))))
+        (swap! state update :y (fn [y] (limit-panning (- y (.-deltaY e)))))))
     (.batchDraw stage)))
+
+
+(defn- on-drag [e state]
+  (when (= "Stage" (.getClassName (.-target e)))
+    (swap! state assoc
+           :x (.x (.-target e))
+           :y (.y (.-target e)))))
 
 
 (defn stage-drag-scale-comp []
@@ -37,19 +53,17 @@
     (r/create-class
      {:component-did-mount (fn [])
       :reagent-render      (fn []
-                             [c/stage {:width       (.-innerWidth js/window)
-                                       :height      (.-innerHeight js/window)
-                                       :draggable   true
-                                       :ref         #(swap! state assoc :stage %)
-                                       :scale-x     (:scale @state)
-                                       :scale-y     (:scale @state)
-                                       :x           (:x @state)
-                                       :y           (:y @state)
-                                       :on-wheel    #(wheel % state)
-                                       :on-drag-end #(when (= "Stage" (.getClassName (.-target %)))
-                                                       (swap! state assoc
-                                                              :x (.x (.-target %))
-                                                              :y (.y (.-target %))))}
+                             [c/stage {:width         (.-innerWidth js/window)
+                                       :height        (.-innerHeight js/window)
+                                       :draggable     true
+                                       :ref           #(swap! state assoc :stage %)
+                                       :scale-x       (:scale @state)
+                                       :scale-y       (:scale @state)
+                                       :x             (:x @state)
+                                       :y             (:y @state)
+                                       :on-wheel      #(wheel % state)
+                                       :on-drag-start #(on-drag % state)
+                                       :on-drag-end   #(on-drag % state)}
                               [c/layer
                                [c/shape {:fill         "#00D2FF"
                                          :stroke       "black"
